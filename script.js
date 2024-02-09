8a53255262e93b8d262453a1b8105ee7c54cba79
springk22 <95098216+springk22@users.noreply.github.com>
2024-02-09 16:35:17 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import React, {
-  useState,
-  useCallback,
-  useEffect
-} from "https://cdn.skypack.dev/react@17.0.1";
+useState,
+useCallback,
+useEffect } from
+"https://cdn.skypack.dev/react@17.0.1";
 import ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
 
 
@@ -15,85 +15,85 @@ const ToDoApp = () => {
     localStorage.setItem('items', JSON.stringify(todos));
   }, [todos]);
 
-  const toggleDone = (index) => {
+  const toggleDone = index => {
     const newTodos = [...todos];
     newTodos[index].done = !newTodos[index].done;
     setTodos(newTodos);
   };
 
-  const deleteTodo = (index) => {
+  const deleteTodo = index => {
     const newTodos = todos.filter((_, i) => i !== index);
     setTodos(newTodos);
   };
 
-  const submit = (event) => {
+  const submit = event => {
     event.preventDefault();
     setTodos([...todos, { description: textInput, done: false }]);
     setTextInput('');
   };
 
-  return (
-    <div className="todoapp-container">
-      <div className="header">
-        <h1>ToDo-App</h1>
-        <h2>Offene Todos: {todos.filter((todo) => !todo.done).length}</h2>
-        <form className="todo-form" onSubmit={submit}>
-          <input
-            type="text"
-            placeholder="Neues Todo..."
-            className="todo-input"
-            onChange={(e) => setTextInput(e.target.value)}
-            value={textInput}
-          />
-          <input type="submit" value="Add Todo" className="add-button" />
-        </form>
-      </div>
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "todoapp-container" }, /*#__PURE__*/
+    React.createElement("div", { className: "header" }, /*#__PURE__*/
+    React.createElement("h1", null, "ToDo-App"), /*#__PURE__*/
+    React.createElement("h2", null, "Offene Todos: ", todos.filter(todo => !todo.done).length), /*#__PURE__*/
+    React.createElement("form", { className: "todo-form", onSubmit: submit }, /*#__PURE__*/
+    React.createElement("input", {
+      type: "text",
+      placeholder: "Neues Todo...",
+      className: "todo-input",
+      onChange: e => setTextInput(e.target.value),
+      value: textInput }), /*#__PURE__*/
 
-      <ToDoList todos={todos} toggleDone={toggleDone} deleteTodo={deleteTodo} />
-    </div>
-  );
+    React.createElement("input", { type: "submit", value: "Add Todo", className: "add-button" }))), /*#__PURE__*/
+
+
+
+    React.createElement(ToDoList, { todos: todos, toggleDone: toggleDone, deleteTodo: deleteTodo })));
+
+
 };
 
 
 const ToDoItem = ({ item, index, toggleDone, deleteTodo }) => {
-  return (
-    <div
-      className={`todo-item ${item.done ? 'done-todo' : ''}`}
-      onClick={() => toggleDone(index)}
-    >
-      <h1 className="text-lg cursor-pointer">{item.description}</h1>
-      <button
-        className="delete-button"
-        onClick={(e) => {
-          e.stopPropagation();
-          deleteTodo(index);
-        }}
-      >
-        Löschen
-      </button>
-    </div>
-  );
+  return /*#__PURE__*/(
+    React.createElement("div", {
+      className: `todo-item ${item.done ? 'done-todo' : ''}`,
+      onClick: () => toggleDone(index) }, /*#__PURE__*/
+
+    React.createElement("h1", { className: "text-lg cursor-pointer" }, item.description), /*#__PURE__*/
+    React.createElement("button", {
+      className: "delete-button",
+      onClick: e => {
+        e.stopPropagation();
+        deleteTodo(index);
+      } }, "L\xF6schen")));
+
+
+
+
+
 };
 
 
 
 const ToDoList = ({ todos, toggleDone, deleteTodo }) => {
-  return (
-    <div>
-      {todos.map((item, index) => (
-        <ToDoItem
-          key={index}
-          item={item}
-          index={index}
-          toggleDone={toggleDone}
-          deleteTodo={deleteTodo}
-        />
-      ))}
-    </div>
-  );
+  return /*#__PURE__*/(
+    React.createElement("div", null,
+    todos.map((item, index) => /*#__PURE__*/
+    React.createElement(ToDoItem, {
+      key: index,
+      item: item,
+      index: index,
+      toggleDone: toggleDone,
+      deleteTodo: deleteTodo }))));
+
+
+
+
 };
 
 
 
 
-ReactDOM.render(<ToDoApp />, document.getElementById("root"));
+ReactDOM.render( /*#__PURE__*/React.createElement(ToDoApp, null), document.getElementById("root"));
